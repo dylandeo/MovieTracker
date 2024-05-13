1162bd5abba69809f3aa041bb9d11ce67247a1a5
Dylan Deokinandan <dylandeo817@gmail.com>
2024-05-13 00:43:06 -0400

    Search page returns movies!!

--- moviemunchies/src/App.js
+++ moviemunchies/src/App.js
@@ -1,42 +1,22 @@
 import React, {useState} from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 import LikesPage from './components/LikesPage';
 import WatchLaterPage from './components/WatchLaterPage';
 import SearchPage from './components/SearchPage';
 import NavBar from './components/NavBar';
-import MovieList from './components/MovieList';
+
 import { Route, Routes  } from "react-router-dom";
 
 
 function App() {
 
-  const [movies, setMovies] = useState([
-    {
-      "Title": "Star Wars: Episode IV - A New Hope",
-      "Year": "1977",
-      "imdbID": "tt0076759",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg"
-  },
-  {
-      "Title": "Star Wars: Episode V - The Empire Strikes Back",
-      "Year": "1980",
-      "imdbID": "tt0080684",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-  },
-  {
-      "Title": "Star Wars: Episode VI - Return of the Jedi",
-      "Year": "1983",
-      "imdbID": "tt0086190",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
-  }
-  ]);
+  
 
   return (
   <>
   <NavBar />
-    <div className="container"> 
+    <div className="_container"> 
       <Routes>
         <Route path="/" element={<SearchPage />}/>
         <Route path="/likes" element={<LikesPage />}/>
@@ -44,9 +24,7 @@ function App() {
       </Routes>
     </div>
 
-    <div>
-        <MovieList movies = {movies}/>
-    </div>
+    
   </>
   )
 }
