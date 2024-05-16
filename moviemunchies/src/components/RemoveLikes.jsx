4f0fddc8e145d70edf7d8e9622a5d031fd8af0cf
Dylan Deokinandan <dylandeo817@gmail.com>
2024-05-15 20:51:13 -0400

    Changed the icon of remove from likes

--- moviemunchies/src/components/RemoveLikes.jsx
+++ moviemunchies/src/components/RemoveLikes.jsx
@@ -6,9 +6,9 @@ const RemoveLikes = () =>
     {
         return (
             <>
-            <span className="m-2">Remove Like</span>
-            <svg  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="blue" class="bi bi-heart-fill" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
+            <span className="m-2">Remove from Likes</span>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-heartbreak-fill" viewBox="0 0 16 16">
+                <path d="M8.931.586 7 3l1.5 4-2 3L8 15C22.534 5.396 13.757-2.21 8.931.586M7.358.77 5.5 3 7 7l-1.5 3 1.815 4.537C-6.533 4.96 2.685-2.467 7.358.77"/>
             </svg>
             </>
         )
