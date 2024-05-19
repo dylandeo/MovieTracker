db9d48865a2c81aefd6915cdb5c842818dc370d1
Linus Trahair <62718508+Ltrahair1@users.noreply.github.com>
2024-05-19 12:29:40 -0400

    Updated App.js to work with mongodb

--- moviemunchies/src/App.js
+++ moviemunchies/src/App.js
@@ -10,10 +10,100 @@ import NavBar from './components/NavBar';
 import SearchBar from './components/SearchBar';
 import AddLikes from './components/AddLikes';
 
+
 function App() {
-    const [movies, setMovies] = useState([]);
+    //states that are needed for mongo server
     const [likes, setLikes] = useState([]);
     const [watchLater, setWatchLater] = useState([]);
+
+    //this function adds all watch later movies when page is loaded
+    const LLater =async (list)=>{
+        //list to be set
+        let lis=[];
+        //for each element in the list, fetch and only set likes when
+        //the list is complete
+        for(let i=0;i<list.length;i++){
+            fetch(`http://www.omdbapi.com/?i=${list[i]}&apikey=ac6774f1`).then((data)=>{
+                return data.json();
+            }).then((d)=>{
+                lis.push(d);
+                if(lis.length===list.length){
+                    setLikes(lis);
+                }
+            });
+            
+        }
+
+    }
+    
+    //this function adds all liked movies when page is loaded
+    const wLater =async (list)=>{
+        //list to be completed
+        let lis=[];
+        //for each element in list, fetch and apply when it is full.
+        for(let i=0;i<list.length;i++){
+            fetch(`http://www.omdbapi.com/?i=${list[i]}&apikey=ac6774f1`).then((data)=>{
+                return data.json();
+            }).then((d)=>{
+                lis.push(d);
+                if(lis.length==list.length){
+                    setWatchLater(lis);
+                }
+            });
+            
+        }
+
+    }
+    //mongo id for user
+    const [mId,setmId ]=useState(-1);
+    //checks if setup is complete
+    const [didMount, setdidMount]=useState(false);
+
+    //finds the next unused id from mongo. It will always be one higher than the max
+    const findID = async ()=>{
+        //if the id doesn't exist, then set it to findid
+        if(localStorage.getItem("mId")===null){
+        var dd;
+        const x= await fetch("http://localhost:4000/api/findid").then((data)=>{
+        return data.json();
+        }).then((d)=>{
+        dd=d.num;
+        localStorage.setItem("mId",dd);
+        });
+        setmId(dd);
+        
+        //creates a new user for the new id
+        const raw =await fetch("http://localhost:4000/api/cUser",{method:'POST',
+        headers:{
+            'Accept':'application/json',
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify({id:dd})
+    });
+        }
+        // if the id already exists, then set both watchlater and likes to the mongodb data.
+        else{
+        setmId(Number(localStorage.getItem("mId")));
+        const raw2=fetch(`http://localhost:4000/api/getUserList?watch=true&id=${Number(localStorage.getItem("mId"))}`).then((data)=>{
+            return data.json();
+        }).then(
+        (d)=>{
+            wLater(d);
+        } 
+        );
+        const raw3=fetch(`http://localhost:4000/api/getUserList?watch=false&id=${Number(localStorage.getItem("mId"))}`).then((data)=>{
+            return data.json();
+        }).then(
+        (d)=>{
+            LLater(d);
+        } 
+        );
+
+        }
+    }
+    
+    const [movies, setMovies] = useState([]);
+    
     const [searchValue, setSearchValue] = useState('');
     const location = useLocation();
 
@@ -22,16 +112,79 @@ function App() {
         const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=ac6774f1`;
         const response = await fetch(url);
         const response_json = await response.json();
-
+        /*if(localStorage.getItem("userid")===null){
+            let idd=findID();
+            localStorage.setItem("userid",idd);
+            createUser(idd);
+        }
+        else{
+            
+        }*/
         if (response_json.Search) {
             setMovies(response_json.Search);
         } else {
             setMovies([]);
         }
     };
+    
+    
+    const createUser=(id)=>{
 
+
+    }
+    //sets mongodb likes to current like list when changed
+    const transferLikes=()=>{
+        //gets list of imdbids
+        let list=[];
+        for(var i=0;i<likes.length;i++){
+            list.push(likes[i].imdbID);
+        }
+        //posts list
+        fetch("http://localhost:4000/api/updateUser",{method:'POST',
+        headers:{
+            'Accept':'application/json',
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify({like:"true",watch:"false",id:mId,upd:list})
+    });
+    }
+    //sets mongodb watchlater to current list when changed
+    const transferWatch=()=>{
+        //gets list of imdbids
+        let list=[];
+        for(var i=0;i<watchLater.length;i++){
+            list.push(watchLater[i].imdbID);
+        }
+        //posts list
+        fetch("http://localhost:4000/api/updateUser",{method:'POST',
+        headers:{
+            'Accept':'application/json',
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify({like:"false",watch:"true",id:mId,upd:list})
+    });
+    }
+    //the initialization function that plays when page
+    //is loaded
+    useEffect(()=>{
+        if(!didMount){
+        findID();
+        setdidMount(true);
+        }
+    },[]);
+    //runs when likes change
+    useEffect(()=>{
+        transferLikes();
+    },[likes]);
+    //runs when watchlist changes
+    useEffect(()=>{
+        transferWatch();
+    },[watchLater]);
+    
     useEffect(() => {
+        
         getMovieRequest(searchValue);
+        console.log(mId);
     }, [searchValue]);
 
     const addToLikes = (movie) => {
